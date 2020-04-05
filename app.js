94eed0c33b1ced645955082db27954c7d2c4ef88
aimigi <lo_min@163.com>
2020-04-05 10:39:11 +0800

    files

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const fs =require('fs')
+const fs = require('fs')
 
-var t={
-    "t":123
-}
+
+const Vue = require('vue')
+const server = require('express')()
+const renderer = require('vue-server-renderer').createRenderer({
+    template: require('fs').readFileSync('./src/index.template.html', 'utf-8')
+})
+
+server.get('*', (req, res) => {
+    const app = new Vue({
+        data: {
+            url: req.url
+        },
+        template: `<div>access url :{{url}}</div>`
+    })
+    renderer.renderToString(app, (err, html) => {
+        // res.end()
+
+        console.log(html)
+    })
+})
+const 
